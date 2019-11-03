066e569b1fd7735490dace2f80013812a48ed88a
Mykola Dekhtiarenko <mdekhtiarenko@ukma.edu.ua>
2019-11-03 12:31:17 +0200

    Updates

---
+++ precache-manifest.c30217d6a26cd3db3f0d33591a99e5f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66977f1ddc097151af86548bf68e602f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce1636a927c9bbfa619d",
+    "url": "/monsters-rolodex/static/css/main.7364e36b.chunk.css"
+  },
+  {
+    "revision": "275c0bfe361a90a19b92",
+    "url": "/monsters-rolodex/static/js/2.a707477e.chunk.js"
+  },
+  {
+    "revision": "ce1636a927c9bbfa619d",
+    "url": "/monsters-rolodex/static/js/main.6d35dfd8.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
